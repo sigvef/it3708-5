7a1cc8488e1b2618ad48129f6265c52599110552
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-04-23 17:31:33 +0200

    Confirmed correctness through empirical testing

--- src/main.c
+++ src/main.c
@@ -11,15 +11,9 @@ int main(){
 
     Node** nodes = NULL;
     int nodes_length = 0;
-    printf("going to read graph\n");
     read_graph("res/two-leaders.txt", &nodes, &nodes_length);
-    printf("read graph finished\n");
 
-    print_graph(nodes, nodes_length);
-
-    printf("going to mis\n");
     int number_of_leaders = mis(nodes, nodes_length);
-    printf("done with mis\n");
 
     printf("leaders: ");
     for(int i = nodes_length - number_of_leaders; i < nodes_length; i++){
--- src/mis.c
+++ src/mis.c
@@ -11,10 +11,12 @@ void swap(Node** array, int i, int j){
 }
 
 bool random(double p){
-    return p < (double) rand() / RAND_MAX;
+    return p > (double) rand() / RAND_MAX;
 }
 
 int mis(Node** nodes, int nodes_length){
+
+    printf("starting mis!\n");
 
     /* find maximum degree of graph */
     int maximum_degree = 0;
@@ -23,8 +25,11 @@ int mis(Node** nodes, int nodes_length){
             maximum_degree = nodes[i]->neighbors_length;
         }
     }
+    
+    printf("maximum degree: %d\n", maximum_degree);
 
-    double p = 1 / maximum_degree;
+    double p = 1. / maximum_degree;
+    printf("p: %f\n", p);
 
     int number_of_active_nodes = nodes_length;
     int number_of_leaders = 0;
@@ -32,44 +37,85 @@ int mis(Node** nodes, int nodes_length){
 
     int M = 34;
     int m_log2_n = 34 * log((double)nodes_length) / log(2.);
+    printf("m_log2_n: %i\n", m_log2_n);
 
-    for(int i=0;i<m_log2_n;i++){
+    while(p < 1) {
 
-        if(p > 1) {
-            return number_of_leaders;
-        }
+        printf("acnodes: %i \n", number_of_active_nodes);
 
-        /* round 1 */
-        for(int j=0;j<number_of_active_nodes;j++){
-            Node* node = nodes[j];
+        for(int i=0;i<m_log2_n;i++){
 
-            if(random(p)){
-                for(int k=0;k<node->neighbors_length;k++){
-                    Node* neighbor = node->neighbors[k];
-                    neighbor->state = 0;
-                    neighbor->has_received_signal = 1;
+            //printf("\n\n==== started mlog2n %i ====\n\n", i);
+
+            /* round 1 */
+            //printf("-- round 1! --\n");
+            for(int j = number_of_non_leaders;
+                    j < number_of_non_leaders + number_of_active_nodes;
+                    j++){
+                Node* node = nodes[j];
+
+                if(random(p)){
+                    printf("triggered! Node: %i\n", node->name);
+                    for(int k=0;k<node->neighbors_length;k++){
+                        Node* neighbor = node->neighbors[k];
+                        neighbor->state = 0;
+                        neighbor->has_received_signal = 1;
+                    }
+                    if(!node->has_received_signal){
+                        node->state = 1;
+                    }
                 }
-                if(!node->has_received_signal){
-                    node->state = 1;
+                node->has_received_signal = 0;
+            }
+
+            /* reset signals */
+            int reset_needed = 0;
+            for(int j = number_of_non_leaders;
+                    j < number_of_non_leaders + number_of_active_nodes;
+                    j++){
+                if(nodes[j]->has_received_signal){
+                    reset_needed = 1; 
+                }
+                nodes[j]->has_received_signal = 0;
+            }
+            if(reset_needed){
+                printf("reset\n");
+            }
+
+            /* round 2 */
+            //printf("\n-- round 2! --\n");
+            for(int j = number_of_non_leaders;
+                    j < number_of_non_leaders + number_of_active_nodes;
+                    j++){
+                Node* node = nodes[j];
+                if(node->state == 1){
+                    for(int k=0;k<node->neighbors_length;k++){
+                        Node* neighbor = node->neighbors[k];
+                        neighbor->has_received_signal = 1;
+                    }
+                    printf("[%i (%i) %i] made node %i a LEADER\n", number_of_non_leaders, j, number_of_leaders, node->name);
+                    printf("before: ");
+                    print_graph(nodes, nodes_length);
+                    swap(nodes, j--, nodes_length - number_of_leaders++ - 1);
+                    number_of_active_nodes--;
+                    printf("after:  ");
+                    print_graph(nodes, nodes_length);
+                    printf("\n");
                 }
             }
-            node->has_received_signal = 0;
-        }
-
-        /* round 2 */
-        for(int j=0;j<number_of_active_nodes;j++){
-            Node* node = nodes[j];
-            if(node->state == 1){
-                for(int k=0;k<node->neighbors_length;k++){
-                    Node* neighbor = node->neighbors[k];
-                    neighbor->has_received_signal = 1;
-                }
-                printf("found a leader: %d\n", node->name);
-                swap(nodes, j--, nodes_length - number_of_leaders++ - 1);
-                number_of_active_nodes--;
-            }else{
+            for(int j = number_of_non_leaders;
+                    j < number_of_non_leaders + number_of_active_nodes;
+                    j++){
+                Node* node = nodes[j];
                 if(node->has_received_signal){
-                    swap(nodes, j--, --number_of_active_nodes);
+                    printf("[%i (%i) %i] made node %i a non-leader\n", number_of_non_leaders, j, number_of_leaders, node->name);
+                    printf("before: ");
+                    print_graph(nodes, nodes_length);
+                    swap(nodes, j, number_of_non_leaders++);
+                    number_of_active_nodes--;
+                    printf("after:  ");
+                    print_graph(nodes, nodes_length);
+                    printf("\n");
                 }
             }
         }
